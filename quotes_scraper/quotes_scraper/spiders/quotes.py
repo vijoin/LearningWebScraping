56f6f1ff4b11bddfb08f2782cdb629cdff320593
Victor Inojosa <vijoin@gmail.com>
2021-06-15 08:50:09 -0300

    Get all quotes follwoing the next linl (if exists) and configure the output for json in the spider

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -6,11 +6,29 @@ class QuotesSpider(scrapy.Spider):
     start_urls = [
         'https://quotes.toscrape.com/page/1/'
     ]
+    custom_settings = {
+        'FEEDS': {
+            'quotes.json': {
+                'format': 'json',
+                'encoding': 'utf8',
+                'store_empty': False,
+                'fields': None,
+                'indent': 4,
+                'item_export_kwargs': {
+                    'export_empty_fields': True,
+                },
+            },
+        },
+    }
 
     def parse(self, response):
-        print("\n\n\n")
-        title = response.xpath("//h1/a/text()").get()
-        quotes = response.xpath("//div[@class='quote']/span[@class='text' and @itemprop='text']/text()").getall()
-        top_ten_tags = response.xpath("//div[contains(@class, 'tags-box')]/span[@class='tag-item']/a/text()").getall()
-        print(title,quotes,top_ten_tags)
-        print("\n\n\n")
+
+        next_link = response.xpath("//ul[@class='pager']/li[@class='next']/a/@href").get()
+        if next_link:
+            yield response.follow(next_link, callback=self.parse)
+
+        yield {
+            'title': response.xpath("//h1/a/text()").get(),
+            'quotes': response.xpath("//div[@class='quote']/span[@class='text' and @itemprop='text']/text()").getall(),
+            'top_ten_tags': response.xpath("//div[contains(@class, 'tags-box')]/span[@class='tag-item']/a/text()").getall(),
+        }
