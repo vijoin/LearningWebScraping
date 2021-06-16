742824dbd38ca04fa2a89849f12a2c853a9e0e09
Victor Inojosa <vijoin@gmail.com>
2021-06-16 08:19:34 -0300

    Allow limit the tags to scrape using spider arguments

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -38,14 +38,21 @@ class QuotesSpider(scrapy.Spider):
 
         title = response.xpath("//h1/a/text()").get()
         quotes = response.xpath("//span[@class='text' and @itemprop='text']/text()").getall()
-        top_ten_tags = response.xpath("//div[contains(@class, 'tags-box')]/span[@class='tag-item']/a/text()").getall()
+        top_tags = response.xpath("//div[contains(@class, 'tags-box')]/span[@class='tag-item']/a/text()").getall()
 
         next_link = response.xpath("//ul[@class='pager']/li[@class='next']/a/@href").get()
         if next_link:
             yield response.follow(next_link, callback=self.parse_quotes_only, cb_kwargs={'quotes': quotes})
 
+        limit_tag = getattr(self, 'limit_tag', None)
+        if limit_tag is not None:
+            try:
+                top_tags = top_tags[:int(limit_tag)]
+            except ValueError as e:
+                raise ValueError("Argument limit_tag must be an integer")
+            
         yield {
             'title': title,
             'quotes': quotes,
-            'top_ten_tags': top_ten_tags,
+            'top_tags': top_tags,
         }
